efa8bce49f2735cddd820d8dd996c81984262e24
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-11-28 19:52:05 +0200

    Files

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -16,11 +16,20 @@ import {
 import WorkSliderBtns from "@/components/workSliderBtns";
 const projects = [
   {
+    num: "01",
     category: "Graduation Project",
     title: "Green Hydrogen Project",
     description:
       "The Green Hydrogen Project focuses on producing hydrogen using renewable energy like solar and wind, offering a clean alternative to fossil fuels. By employing electrolysis, the project aims to create a sustainable energy source with minimal carbon emissions. Key areas include energy storage, safe hydrogen distribution, and applications in transportation, industry, and power. This initiative supports global efforts to reduce greenhouse gas emissions and achieve a carbon-neutral future",
     images: "/Project.jpg",
+  },
+  {
+    num: "02",
+    category: "Product Engineer",
+    title: "Laser Fiber Product Engineer",
+    description:
+      "As a Product Engineer specializing in fiber laser machinery, I focus on precision manufacturing and process optimization. My expertise includes designing solutions for efficient, high-quality laser cutting and engraving, catering to industries like automotive and electronics. I streamline workflows, reduce downtime, and align with modern manufacturing demands for cost-effective and sustainable practices.",
+    images: "/ReyadTany.jpg",
   },
 ];
 const Work = () => {
@@ -45,6 +54,9 @@ const Work = () => {
         <div className="flex flex-col xl:flex-row xl:gap-[30px]">
           <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
             <div className="flex flex-col gap-[30px] h-[50%]">
+              <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+                {project.num}
+              </div>
               <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
                 {project.category}
               </h2>
@@ -74,12 +86,12 @@ const Work = () => {
             >
               {projects.map((project, index) => (
                 <SwiperSlide key={index} className="w-full">
-                  <div className="h-[460px] xl:h-[500px] md:h-[500px] relative group flex justify-center items-center bg-primary rounded-[8px] overflow-hidden">
-                    <div className="absolute inset-0 bg-black bg-opacity-30 group-hover:bg-opacity-50 transition duration-300 z-10 pointer-events-none rounded-[8px]"></div>
+                  <div className="h-[460px] xl:h-[500px] md:h-[500px] relative group flex justify-center items-center bg-primary overflow-hidden rounded-md">
+                    <div className="absolute inset-0 bg-opacity-30 transition duration-300 z-10 pointer-events-none"></div>
                     <Image
                       src={project.images || "/default.jpg"}
                       alt={project.title || "Project"}
-                      className="w-full h-full pointer-events-auto rounded-[8px]"
+                      className="w-full h-full pointer-events-auto"
                       width={500}
                       height={500}
                       style={{
@@ -91,7 +103,11 @@ const Work = () => {
                   </div>
                 </SwiperSlide>
               ))}
-              {/* Add Navigation or Buttons if required */}
+              <WorkSliderBtns
+                containerStyles="flex items-center absolute top-1/2 -translate-y-1/2 w-full justify-between px-4 z-20"
+                btnStyles="p-3 rounded-full shadow-md hover:scale-105 transition"
+                iconStyles="text-white w-6 h-6"
+              />
             </Swiper>
           </div>
         </div>
